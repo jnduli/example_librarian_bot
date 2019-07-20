8f48b99797fe2b32ca7cb0cf5b9f68b0126ab599
John Nduli <yohanaizraeli@gmail.com>
2019-07-20 19:37:09 +0300

    Add more examples to schema

--- schema.sql
+++ schema.sql
@@ -9,9 +9,9 @@ CREATE TABLE librarians (
 CREATE TABLE books (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     librarian INTEGER,
-    name TEST NOT NULL,
+    name TEXT NOT NULL,
     FOREIGN KEY (librarian) REFERENCES librarians(id)
 );
 
 INSERT INTO librarians (name) VALUES ('john'), ('bonface'), ('andrew');
-INSERT INTO books (librarian, name) VALUES (1, 'Sapiens'), (2, 'Homo Deus'), (3, 'The Stoic');
+INSERT INTO books (librarian, name) VALUES (1, 'Sapiens'), (1, 'Courage To Be Disliked'), (1, 'Percy Jackson'), (2, 'Homo Deus'), (2, 'The Bible'), (3, 'The Stoic'), (3, 'Game of Thrones'), (3, 'Harry Potter'), (3, 'Play Things');
